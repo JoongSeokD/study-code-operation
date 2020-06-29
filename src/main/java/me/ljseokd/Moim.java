9614ddbd5fb691c43bd1dee74c5aa7e40423d190
JoongSeokD <ljseokd@gmail.com>
2020-06-29 10:41:21 +0900

    모자에서 토끼를 꺼내는 마술(Feat.ByteBuddy)

---
+++ src/main/java/me/ljseokd/Moim.java
@@ -0,0 +1,16 @@
+package me.ljseokd;
+
+public class Moim {
+    int maxNumberOfAttendees;
+    int numberOfEnrollment;
+
+    public boolean isEnrollmentFull(){
+        if (maxNumberOfAttendees == 0){
+            return false;
+        }
+        if (numberOfEnrollment < maxNumberOfAttendees){
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/main/java/me/ljseokd/Moja.java
@@ -0,0 +1,7 @@
+package me.ljseokd;
+
+public class Moja {
+    public String pullOut(){
+        return "";
+    }
+}
